b29fe10766d513aae3e6fd752ccf517a5b7053db
wesbdss <wesleybenicio4@gmail.com>
2020-07-01 08:49:57 -0300

    :/

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 async def login():
     return {"message":"hello"}
 
-@app.get('/user/(:id)')
-async def userID(id):
-    if id == '123':
+@app.get('/user/{id}')
+async def userID(id: int):
+    if id == 123:
         return {"user":{
             "id":"123",
             "name":"Wesley"}}
