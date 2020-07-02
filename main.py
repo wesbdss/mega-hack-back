2812ac5f2fa6b5df4ddd42c365a8628745d28b77
wesbdss <wesleybenicio4@gmail.com>
2020-07-02 11:14:42 -0300

    att

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 
 livros = [
     {
-        "id": 1,
+        "id": 0,
         "nome": "Amoras",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51TCpYptizL._SY498_BO1,204,203,200_.jpg",
         "autor": "Aldo Fabrini",
@@ -29,7 +29,7 @@ livros = [
         "compra":"https://www.amazon.com.br/dp/8574068365/ref=s9_acsd_hps_bw_c2_x_2_i?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-6&pf_rd_r=ZV9DHPHPA8F0D1S90F0X&pf_rd_t=101&pf_rd_p=ed29da53-5931-4a7a-900e-1bb21e030b2f&pf_rd_i=7844001011"
     },
     {
-        "id": 2,
+        "id": 1,
         "nome": "O poder do hábito",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51VFNJGBvqL._SX346_BO1,204,203,200_.jpg",
         "autor": "Charles Duhigg",
@@ -39,7 +39,7 @@ livros = [
         "tags":["curiosidade","aventura"],
         "compra":"https://www.amazon.com.br/poder-do-h%C3%A1bito-Charles-Duhigg/dp/8539004119/ref=zg_bs_books_9?_encoding=UTF8&psc=1&refRID=FQ04Q6HA39W27CEYK1WE"
     },{
-        "id": 3,
+        "id": 2,
         "nome": "O Pequeno Príncipe Preto",
         "image": "https://images-na.ssl-images-amazon.com/images/I/41KF1iRHe4L._SX373_BO1,204,203,200_.jpg",
         "autor": "Rodrigo França",
@@ -49,7 +49,7 @@ livros = [
         "tags":["curiosidade","aventura"],
         "compra":"https://www.amazon.com.br/Pequeno-Pr%C3%ADncipe-Preto-Rodrigo-Fran%C3%A7a/dp/8520938388/ref=pd_sbs_14_2/132-0412438-4892949?_encoding=UTF8&pd_rd_i=8520938388&pd_rd_r=472ed92d-e2e0-4a64-9a1d-0cf0ec8cb375&pd_rd_w=klIL3&pd_rd_wg=DTddt&pf_rd_p=27be8476-6095-40f6-b57d-3e82cf55061c&pf_rd_r=8N8Z46WM5VBQB69YSS06&psc=1&refRID=8N8Z46WM5VBQB69YSS06"
     },{
-        "id": 4,
+        "id": 3,
         "nome": "Meu crespo é de rainha",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51u4iWzjufL._SX258_BO1,204,203,200_.jpg",
         "autor": "Bell Hooks",
@@ -59,7 +59,7 @@ livros = [
         "tags":["curiosidade","aventura"],
         "compra":"https://www.amazon.com.br/Meu-Crespo-Rainha-Bell-Hooks/dp/857559608X/ref=pd_sbs_14_3/132-0412438-4892949?_encoding=UTF8&pd_rd_i=857559608X&pd_rd_r=472ed92d-e2e0-4a64-9a1d-0cf0ec8cb375&pd_rd_w=klIL3&pd_rd_wg=DTddt&pf_rd_p=27be8476-6095-40f6-b57d-3e82cf55061c&pf_rd_r=8N8Z46WM5VBQB69YSS06&psc=1&refRID=8N8Z46WM5VBQB69YSS06"
     },{
-        "id": 5,
+        "id": 4,
         "nome": "Malala",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51d3aO1O2zL._SX342_BO1,204,203,200_.jpg",
         "autor": "Adriana Carranca ",
@@ -69,7 +69,7 @@ livros = [
         "tags":["curiosidade","aventura"],
         "compra":"https://www.amazon.com.br/Malala-menina-queria-para-escola/dp/8574066702/ref=pd_sbs_14_1/132-0412438-4892949?_encoding=UTF8&pd_rd_i=8574066702&pd_rd_r=b33be874-611f-4aab-b54e-8950b1abdf24&pd_rd_w=bC83e&pd_rd_wg=tfyk2&pf_rd_p=27be8476-6095-40f6-b57d-3e82cf55061c&pf_rd_r=MP54MW1VG6NJW6V48EHB&psc=1&refRID=MP54MW1VG6NJW6V48EHB"
     },{
-        "id": 6,
+        "id": 5,
         "nome": "Capitão Cueca",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51H0BmRLfBL._SX327_BO1,204,203,200_.jpg",
         "autor": "Dav Pilkey",
